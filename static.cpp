31cf3bac68d2808fadb310be26ce9a9a595bb5c2
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id>
2026-05-05 18:10:41 +0700

    membuat dan mengimplementasikan prosedur

--- static.cpp
+++ static.cpp
@@ -20,3 +20,11 @@ public:
     }
 };
 
+//memberi value ke variabel nim dari class mahasiswa
+int Mahasiswa::nim = 10;
+
+//mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
