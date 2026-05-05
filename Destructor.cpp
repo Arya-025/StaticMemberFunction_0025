04a3da73df11905b024a66a270a0a2c10fd0c50d
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id>
2026-05-05 18:34:59 +0700

    memanggil destructor

--- Destructor.cpp
+++ Destructor.cpp
@@ -49,7 +49,7 @@ int main()
 {
     angka belajarcpp(3);                // Constructor Dipanggil
     angka *ptrBelajarcpp = new angka(5); // Constructor Dipanggil
-    
+    delete ptrBelajarcpp;               // Destructor Dipanggil
 
     return 0;
 }
