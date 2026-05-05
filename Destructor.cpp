6fdd41e9ffc7830099f61ec9528517c0b4c4e0a8
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id>
2026-05-05 18:30:13 +0700

    membuat destructor

--- Destructor.cpp
+++ Destructor.cpp
@@ -20,3 +20,9 @@ angka::angka(int i){ // Constructor
     isiData();
 }
 
+angka::~angka(){ // Destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
