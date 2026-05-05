3372bc9600b12d5c08eadf906473ad0329c058ad
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id>
2026-05-05 18:23:11 +0700

    membuat object

--- StaticFunction.cpp
+++ StaticFunction.cpp
@@ -34,3 +34,17 @@ void Mahasiswa::setID() {
     id = ++nim;
 }
 
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    //pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+    //memberi nilai pada setNim() untuk merubah nilai NIM
+  
+    return 0;
+}
